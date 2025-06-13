cd801ef0c5ed99447034a92ac467a1b6447cbd86
Andrey Denisov <and.denisov26@gmail.com>
2025-06-13 11:12:21 +0300

    Настроил отношения между моделями

--- auth/go.mod
+++ auth/go.mod
@@ -5,7 +5,7 @@ go 1.23.0
 toolchain go1.23.10
 
 require (
-	github.com/andro-kes/Chat/shared v0.0.0-20250612111021-a4b4825f9c82
+	github.com/andro-kes/Chat/shared v0.0.0-20250612115720-fe34c5e7a2b9
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.10.1
 	github.com/google/uuid v1.6.0
@@ -31,7 +31,6 @@ require (
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
--- shared/models/chat.go
+++ shared/models/chat.go
@@ -11,7 +11,8 @@ var DB *gorm.DB
 
 type Room struct {
 	gorm.Model
-	Admin User
+	AdminID uint
+	Admin User `gorm:"foreignKey:AdminID"`
 	Users []*User `gorm:"many2many:room_users"`
 	Messages []*Message `gorm:"foreignKey:RoomID"`
 	Broadcast chan *Message
@@ -21,31 +22,35 @@ type Room struct {
 }
 
 func (room *Room) Run() {
-	select {
-	case msg := <-room.Broadcast:
-		room.Mu.Lock()
-		msg.Room = room
-		DB.Create(msg)
-		msg.Save()
-		room.Mu.Unlock()
-	case user := <-room.Registered:
-		ok := slices.Contains(room.Users, user)
-		if !ok {
+	for {
+		select {
+		case msg := <-room.Broadcast:
 			room.Mu.Lock()
-			err := DB.Model(&room).Association("Users").Append(user)
-			if err != nil {
-				log.Println("Models chat error: Не удалось добавить пользователя")
-			}
+			msg.Room = room
+			DB.Create(msg)
+			msg.Save()
 			room.Mu.Unlock()
+		case user := <-room.Registered:
+			ok := slices.Contains(room.Users, user)
+			if !ok {
+				room.Mu.Lock()
+				err := DB.Model(&room).Association("Users").Append(user)
+				if err != nil {
+					log.Println("Models chat error: Не удалось добавить пользователя")
+				}
+				room.Mu.Unlock()
+			}
+		case user := <-room.Unregistered:
+			user.Conn.Close()
 		}
-	case user := <-room.Unregistered:
-		user.Conn.Close()
 	}
 }
 
 type Message struct {
 	gorm.Model
-	Sender User
+	SenderID uint
+	Sender User `gorm:"foreignKey:SenderID"`
+	RoomID uint
 	Room *Room `gorm:"foreignKey:RoomID"`
 	Text string
 }
